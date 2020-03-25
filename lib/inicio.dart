6865a3b2b7c2e268f2541bafc5e3c2fdf02cc345
Rafael Bernal <rabernet@gmail.com>
2020-03-25 11:38:43 -0500

    Create inicio.dart

---
+++ lib/inicio.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_1/components/clipper.dart';
+
+class Entrada2 extends StatelessWidget {
+  const Entrada2({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          ClipPath(
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  clipper: MyClipper3(),
+                  child: Container(
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.height * 0.65,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFed344a),
+                    ),  
+                    child: Opacity(
+                      opacity: 0.25,
+                      child: Container(
+                            decoration: BoxDecoration(
+                            image: const DecorationImage(
+                              image: AssetImage('assets/images/furniture.png', )
+                            ),
+                          )   
+                      )
+                    ),
+                ),
+              ),
+          
+          SingleChildScrollView(
+            padding: const EdgeInsets.only(top: 0, bottom: 20.0),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal:20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                 SizedBox(height:50.0),
+                 Container(
+                   height: 400,
+                   decoration: BoxDecoration(
+                      image: const DecorationImage(
+                        image: AssetImage('assets/images/persondog.png', )
+                      ),
+                    )   
+                 ),
+                SizedBox(height:30.0),
+                Text('Llevalo contigo donde \n  quiera que vayas', textAlign: TextAlign.center, style: TextStyle(fontSize: 30.0, color: Colors.black, fontWeight: FontWeight.w900,),),
+                SizedBox(height:20.0),
+                Text('Busca y agrega los\n  mejores lugares para pasearlo', textAlign: TextAlign.center, style: TextStyle(fontSize: 20.0, color: Colors.black87,),),
+                SizedBox(height:30.0),
+                SizedBox(
+                  width: double.infinity,
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+                    color: Color(0xFFed344a),
+                    textColor: Colors.white,
+                    child: Text("Registrarse", style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.normal,
+                    ),),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 16.0,
+                      horizontal: 32.0
+                    ),
+                    onPressed: (){}
+                    ),
+
+                ),
+                SizedBox(height:10.0),
+                Text('Crear Nueva Cuenta', textAlign: TextAlign.center, style: TextStyle(fontSize: 20.0, color: Color(0xFFed344a), fontWeight: FontWeight.w900),),
+
+                ],
+              ),
+            )
+          )
+        ],
+      ),
+    );
+  }
+
+
+}
